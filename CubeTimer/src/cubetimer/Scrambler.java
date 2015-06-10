897efb968252972e05381debce98b5261e7bb018
kianhighland <kianhighland@cox.net>
2015-06-10 07:15:30 -0700

    Fixed the Move the Same Face Two Times Error
    If figured out that if it wants to move it two times it won't but if it
    want to move the same face three times it will. This is my fault not the
    computers and I fixed the error.

--- CubeTimer/src/cubetimer/Scrambler.java
+++ CubeTimer/src/cubetimer/Scrambler.java
@@ -6,6 +6,7 @@ public class Scrambler {
 	String randomScramble;
 	String randomMove;
 	String lastFaceMoved;
+	boolean faceMoving;
 	Random random;
 
 	public Scrambler() {
@@ -19,54 +20,61 @@ public class Scrambler {
 			if (randomNumber == 0) {
 				if (lastFaceMoved == "R") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "R";
 					lastFaceMoved = "R";
+					faceMoving = true;
+					
 				}
 
 			} else if (randomNumber == 1) {
 				if (lastFaceMoved == "L") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "L";
 					lastFaceMoved = "L";
+					faceMoving = true;
 				}
 			} else if (randomNumber == 2) {
 				if (lastFaceMoved == "U") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "U";
 					lastFaceMoved = "U";
+					faceMoving = true;
 				}
 			} else if (randomNumber == 3) {
 				if (lastFaceMoved == "D") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "D";
 					lastFaceMoved = "D";
+					faceMoving = true;
 				}
 			} else if (randomNumber == 4) {
 				if (lastFaceMoved == "F") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "F";
 					lastFaceMoved = "F";
+					faceMoving = true;
 				}
 			} else if (randomNumber == 5) {
 				if (lastFaceMoved == "B") {
 					i = i - 1;
-					lastFaceMoved = null;
+					faceMoving = false;
 				} else {
 					randomMove = "B";
 					lastFaceMoved = "B";
+					faceMoving = true;
 				}
 			}
-			if (lastFaceMoved != null) {
+			if (faceMoving) {
 				if (i == 0) {
 					randomScramble = randomMove;
 				} else {
